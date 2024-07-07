1cfb030358d3fe8a8cd612782e3c3a3aeb58af21
fredbern <fre19036@byui.edu>
2024-07-07 18:08:43 +1100

    update

--- routes/items.js
+++ routes/items.js
@@ -5,8 +5,8 @@ const itemController = require('../controllers/items');
 const dublication = require('../middleware/checkDublication');
 const { isAuthenticated } = require("../middleware/authenticate");
 
-router.get('/', itemController.getAll);
-router.get('/:id', itemController.getSingle);
+router.get('/', isAuthenticated, itemController.getAll);
+router.get('/:id', isAuthenticated, itemController.getSingle);
 router.post('/', isAuthenticated, dublication.checkDuplicateTagNumber, itemController.createItem);
 router.put('/:id', isAuthenticated, dublication.checkDuplicateTagNumber, itemController.updateItem);
 router.delete('/:id', isAuthenticated, itemController.deleteItem);
