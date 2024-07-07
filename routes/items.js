7cda203ff084c2d6e5f25f73adbf3fbc2827f75e
fredbern <fre19036@byui.edu>
2024-07-07 20:54:38 +1100

    update items

--- routes/items.js
+++ routes/items.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// const validation = require('../middleware/validate');
+const { saveItem } = require('../middleware/validate');
 const itemController = require('../controllers/items');
 const dublication = require('../middleware/checkDublication');
 const { isAuthenticated } = require("../middleware/authenticate");
 
 router.get('/', isAuthenticated, itemController.getAll);
 router.get('/:id', isAuthenticated, itemController.getSingle);
-router.post('/', isAuthenticated, dublication.checkDuplicateTagNumber, itemController.createItem);
-router.put('/:id', isAuthenticated, dublication.checkDuplicateTagNumber, itemController.updateItem);
+router.post('/', isAuthenticated, saveItem, dublication.checkDuplicateTagNumber, itemController.createItem);
+router.put('/:id', isAuthenticated, saveItem, dublication.checkDuplicateTagNumber, itemController.updateItem);
 router.delete('/:id', isAuthenticated, itemController.deleteItem);
-// router.post('/', validation.saveItem, dublication.checkDuplicateTagNumber, itemController.createItem);
-// router.put('/:id', validation.saveItem, dublication.checkDuplicateTagNumber, itemController.updateItem);
-// router.delete('/:id', itemController.deleteItem);
 
 module.exports = router;
