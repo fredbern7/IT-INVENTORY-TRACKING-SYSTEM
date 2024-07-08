b80be2c29c783c72cc6aeaddb94171411135b2b0
fredbern <fre19036@byui.edu>
2024-07-08 20:51:11 +1100

    update byscript

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt');
 
 const getAll = async (req, res) => {
   //#swagger-tags-['Users']
@@ -41,14 +42,18 @@ const getSingle = async (req, res) => {
 
 const createUser = async (req, res) => {
   //#swagger-tags-['Users']
-  const user = {
-    firstName: req.body.firstName,
-    middleName: req.body.middleName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  };
+  const { firstName, middleName, lastName, email, password } = req.body;
+
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+      firstName,
+      middleName,
+      lastName,
+      email,
+      password: hashedPassword
+    };
+
     const response = await mongodb
       .getDatabase()
       .collection('users')
@@ -69,26 +74,33 @@ const updateUser = async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     return res.status(400).json('Must use a valid user id to update a user.');
   }
-  const userId = new ObjectId(req.params.id);
-  const user = {
-    firstName: req.body.firstName,
-    middleName: req.body.middleName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  };
 
-  const response = await mongodb
+  const userId = new ObjectId(req.params.id);
+  const { firstName, middleName, lastName, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+      firstName,
+      middleName,
+      lastName,
+      email,
+      password: hashedPassword
+    };
+
+    const response = await mongodb
       .getDatabase()
       .collection('users')
       .replaceOne({ _id: userId }, user);
     console.log(response);
     if (response.modifiedCount > 0) {
       res.status(204).send();
-    console.log(response);
-      } else {
-        res.status(500).json({ message: 'Some error occurred while updating the user.' });
-      }
+    } else {
+      res.status(500).json({ message: 'Some error occurred while updating the user.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const deleteUser = async (req, res) => {
